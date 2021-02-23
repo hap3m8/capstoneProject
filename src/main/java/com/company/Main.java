9b759eaf078da4a9c408d81ab3af3c049ba8a92c
Hayden Phillips <56phillips58@gmail.com>
2021-02-23 15:50:07 -0600

    refactoring VCS

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,3 +1,16 @@
+/*
+Identifier Type	Convention	Examples
+Package	All lowercase	demos.javawebdevelopment, org.launchcode.utilities
+Class	Start with an uppercase letter	Scanner, System, Cello
+Method	Start with a lower case letter, and use camelCase to represent multi-word method names	nextInt(), getId()
+Instance variable	Start with a lowercase letter and use camelCase	id, firstName
+Constant	All uppercase letters, words separated by underscores	MAX_INT */
+
+
+
+
+
+
 package com.company;
 
 public class Main {

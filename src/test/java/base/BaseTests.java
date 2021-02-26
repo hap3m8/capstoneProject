729f76cfd65a9970eb9dbfbc5fde6805bc9bc6b0
Hayden Phillips <56phillips58@gmail.com>
2021-02-25 22:46:50 -0600

    rest of project files integrated test class added chrome driver and selenium integration

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,25 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTests {
+
+    private WebDriver driver;
+
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://www.youtube.com/watch?v=f02mOEt11OQ");
+        System.out.println(driver.getTitle());
+    }
+
+
+    /* temporary main method*/
+    public static void main(String args[]){
+        BaseTests test = new BaseTests();
+        test.setUp();
+    }
+
+
+}
